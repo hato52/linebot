950ed2a45f8695bfc92b90a32e41e25fc8dc2fcf
服部　颯太 <souta@Hato-MacBook-Air.local>
2019-06-27 23:29:09 +0900

    initial commit

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const line = require("@line/bot-sdk");
+
+const app = express();
+
+const line_config = {
+    channelAccessToken: process.env.LINE_ACCESS_TOKEN,
+    channelSecret: process.env.LINE_CHANNEL_SECRET
+};
+
+app.listen(process.env.PORT || 3000);
+
+app.post('/bot/webhook', line.middleware(line_config), (req, res, next) => {
+    res.sendStatus(200);
+    console.log(req.body);
+});
