614490fd3ade5ddba3ee28e6e9fa3a9b71b65c52
hato52 <hato.deve@gmail.com>
2019-06-30 12:58:17 +0900

    [add] test message

--- index.js
+++ index.js
@@ -10,7 +10,31 @@ const line_config = {
 
 app.listen(process.env.PORT || 3000);
 
+// webhookのルーティング設定
 app.post('/bot/webhook', line.middleware(line_config), (req, res, next) => {
     res.sendStatus(200);
-    console.log(req.body);
+
+    let event_processed = [];
+
+    // イベントオブジェクトを順次処理。
+    req.body.events.forEach((event) => {
+        // この処理の対象をイベントタイプがメッセージで、かつ、テキストタイプだった場合に限定。
+        if (event.type == "message" && event.message.type == "text"){
+            // ユーザーからのテキストメッセージが「こんにちは」だった場合のみ反応。
+            if (event.message.text == "こんにちは"){
+                // replyMessage()で返信し、そのプロミスをevents_processedに追加。
+                events_processed.push(bot.replyMessage(event.replyToken, {
+                    type: "text",
+                    text: "これはこれは"
+                }));
+            }
+        }
+    });
+
+    // すべてのイベント処理が終了したら何個のイベントが処理されたか出力。
+    Promise.all(events_processed).then(
+        (response) => {
+            console.log(`${response.length} event(s) processed.`);
+        }
+    );
 });
