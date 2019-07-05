a4759439e617385ac9d2f2ace8604e3002e8c9c6
hato52 <hato.deve@gmail.com>
2019-07-05 21:50:09 +0900

    [fix] improve

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
-const express = require("express");
-const line = require("@line/bot-sdk");
+const request = require('request');
+const express = require('express');
+const line = require('@line/bot-sdk');
+const CronJob = require('cron').CronJob;
 
 const app = express();
 
@@ -11,7 +13,46 @@ const line_config = {
 app.listen(process.env.PORT || 3000);
 
 // APIコールのためのクライアントインスタンスを作成
-const bot = new line.Client(line_config);
+const client = new line.Client(line_config);
+
+// cronのジョブ設定
+new CronJob('0 */10 6,18 * * *', () => {
+    // 遅延情報の取得とPUSHメッセージの送信
+    request.get('https://tetsudo.rti-giken.jp/free/delay.json', (err,res,body) => {
+        if (err) {
+            console.log(err);
+            return;
+        }
+
+        let delay_flag = false;
+        // 取得したJSONをパースする
+        let train = "";
+        let json = JSON.parse(body);
+        json.forEach((data) => {
+            if (data.name == "京浜東北線" || data.name == "埼京線" || data.name == "京王線") {
+                delay_flag = true;
+                train += ("・" + data.name + "¥n");
+            }
+        });
+
+        // 遅延情報があればPUSHメッセージの送信
+        const message = '';
+        if (delay_flag == true) {
+            message = {
+                type: 'text',
+                text: '現在、以下の交通網に遅延が発生しています¥n' + train
+            };
+
+            client.pushMessage('U98ea37be447321d09ab130f994489f2a', message)
+            .then(() => {
+                console.log("PUSHメッセージの送信完了");
+            })
+            .catch((err) => {
+                console.log(err);
+            });
+        }
+    });
+}, null, true);
 
 // webhookのルーティング設定
 app.post('/bot/webhook', line.middleware(line_config), (req, res, next) => {
@@ -22,13 +63,13 @@ app.post('/bot/webhook', line.middleware(line_config), (req, res, next) => {
     // イベントオブジェクトを順次処理。
     req.body.events.forEach((event) => {
         // この処理の対象をイベントタイプがメッセージで、かつ、テキストタイプだった場合に限定。
-        if (event.type == "message" && event.message.type == "text"){
+        if (event.type == 'message' && event.message.type == 'text'){
             // ユーザーからのテキストメッセージが「こんにちは」だった場合のみ反応。
-            if (event.message.text == "こんにちは"){
+            if (event.message.text == 'こんにちは'){
                 // replyMessage()で返信し、そのプロミスをevents_processedに追加。
-                events_processed.push(bot.replyMessage(event.replyToken, {
-                    type: "text",
-                    text: "これはこれは"
+                events_processed.push(client.replyMessage(event.replyToken, {
+                    type: 'text',
+                    text: 'いえあ'
                 }));
             }
         }
