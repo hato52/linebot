437af5f324a56604c678bec52b6cc9a95de71ec0
hato52 <hato.deve@gmail.com>
2019-07-10 23:31:19 +0900

    [fix] postgresql

--- index.js
+++ index.js
@@ -94,7 +94,7 @@ app.post('/bot/webhook', line.middleware(line_config), (req, res, next) => {
             // グループIDをDBに保存
             const query = {
                 text: 'INSERT INTO destination(id) VALUES($1)',
-                value: event.source.groupId
+                value: [event.source.groupId]
             };
 
             db_client.query(query, (err, res) => {
@@ -111,7 +111,7 @@ app.post('/bot/webhook', line.middleware(line_config), (req, res, next) => {
             // ユーザIDをDBに保存
             const query = {
                 text: 'INSERT INTO destination(id) VALUES($1)',
-                value: event.source.userId
+                value: [event.source.userId]
             };
 
             db_client.query(query, (err, res) => {
@@ -127,7 +127,7 @@ app.post('/bot/webhook', line.middleware(line_config), (req, res, next) => {
             // ユーザIDをDBに保存
             const query = {
                 text: 'INSERT INTO destination(id) VALUES($1)',
-                value: event.source.userId
+                value: [event.source.userId]
             };
 
             db_client.query(query, (err, res) => {
