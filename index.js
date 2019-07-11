80c5400dc1d1c2abf2123ce8e37d0f871787dfe1
hato52 <hato.deve@gmail.com>
2019-07-11 22:16:45 +0900

    [fix] improve

--- index.js
+++ index.js
@@ -27,121 +27,234 @@ const client = new line.Client(line_config);
 // cronのジョブ設定
 // 平日に20分置きに取得 通勤と退勤のタイミングのみ
 new CronJob('0 */20 5-7,17-19 * * 1-5', () => {
-    // 遅延情報の取得とPUSHメッセージの送信
-    request.get('https://tetsudo.rti-giken.jp/free/delay.json', (err,res,body) => {
-        if (err) {
-            console.log(err);
-            return;
-        }
+    const message = get_api('PUSH');
 
-        let delay_flag = false;
-        // 取得したJSONをパースする
-        let train = "";
-        let json = JSON.parse(body);
-        json.forEach((data) => {
-            if (data.name == "京浜東北線" || data.name == "埼京線" || data.name == "京王線" || data.name == "東武東上線" || data.name == "武蔵野線") {
-                delay_flag = true;
-                train += ("\n・" + data.name);
-            }
-        });
-
-        // 遅延情報があればPUSHメッセージの送信
-        if (delay_flag == true) {
-            console.log("遅延が発生しています");
-            const message = {
-                type: 'text',
-                text: '現在、以下の交通網に遅延が発生しています\n' + train
-            };
-
-            client.pushMessage(process.env.LINE_USER_ID, message)
-            .then(() => {
-                console.log("PUSHメッセージの送信完了");
-            })
-            .catch((err) => {
+    if (message == null) {
+        console.log("平常運行です");
+    } else {
+        // メッセージの送信先をDBから取得して送信先文字列を形成
+        db_client.query('SELECT id from destination', (err, res) => {
+            if (err) {
                 console.log(err);
+            }
+            let to = [];
+            res.rows.forEach((row) => {
+                to.push(row);
             });
-        } else {
-            console.log("平常運行です");
-        }
-    });
+        });
+        console.log(to);
+
+        client.multicast(to, message)
+        .then(() => {
+            console.log("PUSHメッセージの送信完了");
+        })
+        .catch((err) => {
+            console.log(err);
+        });
+    }
+    // 遅延情報の取得とPUSHメッセージの送信
+    // request.get('https://tetsudo.rti-giken.jp/free/delay.json', (err,res,body) => {
+    //     if (err) {
+    //         console.log(err);
+    //         return;
+    //     }
+
+    //     let delay_flag = false;
+    //     // 取得したJSONをパースする
+    //     let train = "";
+    //     let json = JSON.parse(body);
+    //     json.forEach((data) => {
+    //         if (data.name == "京浜東北線" || data.name == "埼京線" || data.name == "京王線" || data.name == "東武東上線" || data.name == "武蔵野線") {
+    //             delay_flag = true;
+    //             train += ("\n・" + data.name);
+    //         }
+    //     });
+
+    //     // 遅延情報があればPUSHメッセージの送信
+    //     if (delay_flag == true) {
+    //         console.log("遅延が発生しています");
+    //         const message = {
+    //             type: 'text',
+    //             text: '現在、以下の交通網に遅延が発生しています\n' + train
+    //         };
+
+    //         // メッセージの送信先をDBから取得して送信先文字列を形成
+    //         db_client.query('SELECT id from destination', (err, res) => {
+    //             if (err) {
+    //                 console.log(err);
+    //             }
+    //             // TODO ここの部分をちゃんとする
+    //             let to = [];
+    //             res.rows.forEach((row) => {
+    //                 to.push(row);
+    //             });
+    //         });
+    //         console.log(to);
+
+    //         client.multicast(to, message)
+    //         .then(() => {
+    //             console.log("PUSHメッセージの送信完了");
+    //         })
+    //         .catch((err) => {
+    //             console.log(err);
+    //         });
+    //     } else {
+    //         console.log("平常運行です");
+    //     }
+    // });
 }, null, true);
 
 // webhookのルーティング設定
 app.post('/bot/webhook', line.middleware(line_config), (req, res, next) => {
     res.sendStatus(200);
 
-    let events_processed = [];
-
     // イベントオブジェクトを順次処理。
     req.body.events.forEach((event) => {
+        switch (event.type) {
+            case 'message':
+                if (event.message.type == 'text' && event.message.text == '遅延') {
+                    const messge = get_api('REPLY');
+                    client.replyMessage(event.replyToken, message);
+                } 
+                break;
 
-        // メッセージイベントの取得
-        if (event.type == 'message' && event.message.type == 'text'){
-            // ユーザーからのテキストメッセージが「こんにちは」だった場合のみ反応。
-            if (event.message.text == 'こんにちは'){
-                // replyMessage()で返信し、そのプロミスをevents_processedに追加。
-                events_processed.push(client.replyMessage(event.replyToken, {
-                    type: 'text',
-                    text: 'いえあ'
-                }));
-            }
+            case 'join':
+                console.log(event.source.groupId);
+
+                // グループIDをDBに保存
+                const query = {
+                    text: 'INSERT INTO destination(id) VALUES($1)',
+                    values: [event.source.groupId]
+                };
+    
+                db_client.query(query, (err, res) => {
+                    if (err) {
+                        console.log(err);
+                    }
+                });
+                break;
+
+            case 'follow':
+                console.log(event.source.userId);
+
+                // ユーザIDをDBに保存
+                const query = {
+                    text: 'INSERT INTO destination(id) VALUES($1)',
+                    values: [event.source.userId]
+                };
+    
+                db_client.query(query, (err, res) => {
+                    if (err) {
+                        console.log(err);
+                    }
+                });
+                break;
+
+            default:
+                break;
         }
 
-        // ルーム参加イベントの取得
-        if (event.type == 'join') {
-            console.log(event.source.groupId);
+        // // メッセージイベントの取得
+        // if (event.type == 'message' && event.message.type == 'text'){
+        //     // ユーザーからのテキストメッセージが「こんにちは」だった場合のみ反応。
+        //     if (event.message.text == 'こんにちは'){
+        //         const message = get_api("REPLY");
+        //         client.replyMessage(event.replyToken, message);
+        //         // replyMessage()で返信し、そのプロミスをevents_processedに追加。
+        //         // events_processed.push(client.replyMessage(event.replyToken, {
+        //         //     type: 'text',
+        //         //     text: 'いえあ'
+        //         // }));
+        //     }
+        // }
 
-            // グループIDをDBに保存
-            const query = {
-                text: 'INSERT INTO destination(id) VALUES($1)',
-                values: [event.source.groupId]
-            };
+        // // ルーム参加イベントの取得
+        // if (event.type == 'join') {
+        //     console.log(event.source.groupId);
 
-            db_client.query(query, (err, res) => {
-                if (err) {
-                    console.log(err);
-                }
-            });
-        }
+        //     // グループIDをDBに保存
+        //     const query = {
+        //         text: 'INSERT INTO destination(id) VALUES($1)',
+        //         values: [event.source.groupId]
+        //     };
 
-        // フォローイベントの取得
-        if (event.type == 'follow') {
-            console.log(event.source.userId);
+        //     db_client.query(query, (err, res) => {
+        //         if (err) {
+        //             console.log(err);
+        //         }
+        //     });
+        // }
 
-            // ユーザIDをDBに保存
-            const query = {
-                text: 'INSERT INTO destination(id) VALUES($1)',
-                values: [event.source.userId]
-            };
+        // // フォローイベントの取得
+        // if (event.type == 'follow') {
+        //     console.log(event.source.userId);
 
-            db_client.query(query, (err, res) => {
-                if (err) {
-                    console.log(err);
-                }
-            });
-        }
+        //     // ユーザIDをDBに保存
+        //     const query = {
+        //         text: 'INSERT INTO destination(id) VALUES($1)',
+        //         values: [event.source.userId]
+        //     };
 
-        if (event.type == 'unfollow') {
-            console.log(event.source.userId);
-
-            // ユーザIDをDBに保存
-            const query = {
-                text: 'INSERT INTO destination(id) VALUES($1)',
-                values: [event.source.userId]
-            };
-
-            db_client.query(query, (err, res) => {
-                if (err) {
-                    console.log(err);
-                }
-            });
-        }
+        //     db_client.query(query, (err, res) => {
+        //         if (err) {
+        //             console.log(err);
+        //         }
+        //     });
+        // }
     });
-
-    // すべてのイベント処理が終了したら何個のイベントが処理されたか出力。
-    Promise.all(events_processed).then(
-        (response) => {
-            console.log(`${response.length} event(s) processed.`);
-        }
-    );
 });
+
+// 遅延情報の取得
+function get_api(message_type) {
+    // APIにリクエストを送信
+    request.get('https://tetsudo.rti-giken.jp/free/delay.json', (err,res,body) => {
+        // エラーチェック
+        if (err) {
+            console.log(err);
+            return;
+        }
+
+        // JSONの解析とテキスト生成
+        let train = '';
+        let message;
+        let json = JSON.parse(body);
+        switch(message_type) {
+            // プッシュメッセージ
+            case 'PUSH':
+                let delay_flag = false;
+                json.forEach((data) => {
+                    if (data.name == "京浜東北線" || data.name == "埼京線" || data.name == "京王線" || data.name == "東武東上線" || data.name == "武蔵野線") {
+                        delay_flag = true;
+                        train += ("\n・" + data.name);
+                    }
+                });
+                if (delay_flag == true) {
+                    console.log("遅延が発生しています");
+                    message = {
+                        type: 'text',
+                        text: '現在、以下の交通網に遅延が発生しています\n' + train
+                    };
+                } else {
+                    message = null;
+                }
+                break;
+
+            // リプライメッセージ
+            case 'REPLY':
+                json.forEach((data) => {
+                    train += ("\n・" + data.name);
+                });
+                message = {
+                    type: 'text',
+                    text: '現在、以下の交通網に遅延が発生しています\n' + train
+                };
+                break;
+
+            default:
+                message = null;
+                break;
+        }
+
+        return message;
+    });
+}
