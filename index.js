d649a04f76ad60fc72f8c55d929ef5e4e1d1b4fd
hato52 <hato.deve@gmail.com>
2019-07-21 13:55:31 +0900

    [add] o-n

--- index.js
+++ index.js
@@ -139,6 +139,22 @@ app.post('/bot/webhook', line.middleware(line_config), (req, res, next) => {
                             });
                         });
                     }
+
+                    // おーん
+                    if (event.message.text == 'おーん' || event.message.text == 'おん') {
+                        const message = {
+                            type: 'text',
+                            text: 'おーん'
+                        };
+
+                        client.replyMessage(event.replyToken, message)
+                        .then(() => {
+                            console.log("リプライメッセージの送信完了");
+                        })
+                        .catch((err) => {
+                            console.log(err);
+                        });
+                    }
                 }
                 break;
 
